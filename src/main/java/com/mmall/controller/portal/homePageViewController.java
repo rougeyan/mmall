f415f5c9f0ae7cc881721c0731b437df6a4d0489
rougeyanWindows <se7ne_886@163.com>
2020-06-13 15:19:41 +0800

    增加首页部分接口请求

---
+++ src/main/java/com/mmall/controller/portal/homePageViewController.java
@@ -0,0 +1,31 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.ServiceResponse;
+import com.mmall.service.ICategoryService;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/homePageView/")
+public class homePageViewController {
+    @Autowired
+    private ICategoryService iCategoryService;
+    /**
+     * 根据categoryId 获取当前categoryId  子节点 所有平级category 信息 不递归;
+     * @param session
+     * @param categoryId
+     * @return
+     */
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServiceResponse getChildrenParalleCategory(HttpSession session, @RequestParam(value ="categoryId",defaultValue = "0") int categoryId){
+        // 查询子节点的category信息, 并且不递归 保持平级
+        return iCategoryService.getChildrenParalleCategory(categoryId);
+    }
+}
