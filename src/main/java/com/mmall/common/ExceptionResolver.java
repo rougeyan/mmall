60ffd9cc6cb1cae9e929bd03ca458cea5e07ed8b
rougeyanWindows <se7ne_886@163.com>
2020-06-17 15:50:11 +0800

    全局异常

--- src/main/java/com/mmall/common/ExceptionResolver.java
+++ src/main/java/com/mmall/common/ExceptionResolver.java
@@ -1,16 +1,27 @@
 package com.mmall.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // 处理全局异常
+@Slf4j
+@Component
 public class ExceptionResolver  implements HandlerExceptionResolver {
-
     @Override
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
-        return null;
+        // 记录错误日志；
+        log.error("{}Exception",httpServletRequest.getRequestURI(),e);
+        // 通过modelView
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        modelAndView.addObject("status",ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg","接口异常,请联系管理员");
+        modelAndView.addObject("data",e.toString());
+        return modelAndView;
     }
 }
