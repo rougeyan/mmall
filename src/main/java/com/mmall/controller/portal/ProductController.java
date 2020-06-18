e6917f76e901081282d049bce47e3c76da3dbf23
rougeyanWindows <se7ne_886@163.com>
2020-06-18 14:16:28 +0800

    改造查询的restful接口;

--- src/main/java/com/mmall/controller/portal/ProductController.java
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServiceResponse;
 import com.mmall.service.IProducetService;
 import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product/")
@@ -21,9 +20,11 @@ public class ProductController {
      * @param productId
      * @return
      */
-    @RequestMapping("detail.do")
+    // @RequestMapping("detail.do") http://localhost:8081/product/detail.do?productId=28
+    @RequestMapping(value = "/{productId}",method = RequestMethod.GET)
+    // http://localhost:8081/product/28 相对简洁一些 配合注解 @PathVariable
     @ResponseBody
-    public ServiceResponse<ProductDetailVo> detail (Integer productId){
+    public ServiceResponse<ProductDetailVo> detail (@PathVariable Integer productId){
         return iProducetService.getProductDetail(productId);
     }
 
@@ -44,5 +45,99 @@ public class ProductController {
                                           @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
                                           @RequestParam(value = "orderBy",defaultValue = "") String orderBy){
         return iProducetService.getProductByKeywordCategory(keyword,categoryId,PageNum,pageSize,orderBy);
+    }
+
+    // restful;这里是所有参数都传
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword,
+                                          @PathVariable(value = "categoryId") Integer categoryId,
+                                          @PathVariable(value = "pageNum") Integer pageNum ,
+                                          @PathVariable(value = "pageSize") Integer pageSize,
+                                          @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize== null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProducetService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+    // 没传品类id
+    @RequestMapping(value = "/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "keyword") String keyword,
+                                                 @PathVariable(value = "pageNum") Integer pageNum ,
+                                                 @PathVariable(value = "pageSize") Integer pageSize,
+                                                 @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize== null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProducetService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+    }
+
+    // 没传关键字
+    @RequestMapping(value = "/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "categoryId") Integer categoryId,
+                                                 @PathVariable(value = "pageNum") Integer pageNum ,
+                                                 @PathVariable(value = "pageSize") Integer pageSize,
+                                                 @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize== null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProducetService.getProductByKeywordCategory(null,categoryId,pageNum,pageSize,orderBy);
+    }
+    // 只传品类id(或产品id);
+    @RequestMapping(value = "/category/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId") Integer categoryId,
+                                                 @PathVariable(value = "pageNum") Integer pageNum ,
+                                                 @PathVariable(value = "pageSize") Integer pageSize,
+                                                 @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize== null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProducetService.getProductByKeywordCategory(null,categoryId,pageNum,pageSize,orderBy);
+    }
+
+    // 只传关键字;
+    @RequestMapping(value = "/keyword/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword,
+                                                 @PathVariable(value = "pageNum") Integer pageNum ,
+                                                 @PathVariable(value = "pageSize") Integer pageSize,
+                                                 @PathVariable(value = "orderBy") String orderBy){
+        if(pageNum == null){
+            pageNum = 1;
+        }
+        if(pageSize== null){
+            pageSize = 10;
+        }
+        if(StringUtils.isBlank(orderBy)){
+            orderBy = "price_asc";
+        }
+        return iProducetService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
     }
 }
